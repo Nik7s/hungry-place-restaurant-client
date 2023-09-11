e65d15914e8c13dcf3e77ada3437d0de5f46e195
okshi <johnstarhafiz@gmail.com>
2023-09-11 22:45:55 +0600

    Complete footer 3/1 portion

--- src/Pages/Home/Home/Shared/Footer/Footer.jsx
+++ src/Pages/Home/Home/Shared/Footer/Footer.jsx
@@ -1,17 +1,49 @@
 const Footer = () => {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4  bg-footerBg text-white py-20 px-12">
+    <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-3 gap-5  bg-footerBg text-white py-20 px-12">
       <div>
-        <h1>DIV -1 </h1>
+        <h1 className="font-mono font-bold text-xl mb-10">OPENING HOURS</h1>
+        <div className="grid lg:grid-cols-3">
+          <p className="font-mono font-bold">MONDAY</p>
+          <div className="divider ms-[-80px]"></div>
+          <p className="ms-2 font-mono text-red-600 font-bold">09:00 - 06:00</p>
+        </div>
+        <div className="grid lg:grid-cols-3">
+          <p className="font-mono font-bold">THUESDAY</p>
+          <div className="divider ms-[-65px]"></div>
+          <p className="ms-2 font-mono text-red-600 font-bold">09:00 - 06:00</p>
+        </div>
+        <div className="grid lg:grid-cols-3">
+          <p className="font-mono font-bold">WEDNESDAY</p>
+          <div className="divider ms-[-63px]"></div>
+          <p className="ms-2 font-mono text-red-600 font-bold">09:00 - 06:00</p>
+        </div>
+        <div className="grid lg:grid-cols-3">
+          <p className="font-mono font-bold">THURSDAY</p>
+          <div className="divider ms-[-70px]"></div>
+          <p className="ms-2 font-mono text-red-600 font-bold">09:00 - 06:00</p>
+        </div>
+        <div className="grid lg:grid-cols-3">
+          <p className="font-mono font-bold">FRIDAY</p>
+          <div className="divider ms-[-78px]"></div>
+          <p className="ms-2 font-mono text-red-600 font-bold">09:00 - 06:00</p>
+        </div>
+        <div className="grid lg:grid-cols-3">
+          <p className="font-mono font-bold">SATRURDAY</p>
+          <div className="divider ms-[-68px]"></div>
+          <p className="ms-2 font-mono text-red-600 font-bold">09:00 - 06:00</p>
+        </div>
+        <div className="grid lg:grid-cols-3">
+          <p className="font-mono font-bold">SUNDAY</p>
+          <div className="divider ms-[-80px]"></div>
+          <p className="ms-2 font-mono text-zinc-500 font-bold">CLOSED</p>
+        </div>
       </div>
       <div>
-        <h1>DIV -2</h1>
+        <h1 className="font-mono font-bold text-xl">CONTUCT US</h1>
       </div>
       <div>
-        <h1>DIV -3</h1>
-      </div>
-      <div>
-        <h1>DIV -4</h1>
+        <h1 className="font-mono font-bold text-xl">OTHER LOCATIONS</h1>
       </div>
     </div>
   );
