a3a1474ed1e3dd211bb5fb39932b24297c4980d2
okshi <johnstarhafiz@gmail.com>
2023-09-14 21:05:06 +0600

    Refector Navbar design and code

--- src/Pages/Shared/NavBar/NavBar.jsx
+++ src/Pages/Shared/NavBar/NavBar.jsx
@@ -4,17 +4,7 @@ const NavBar = () => {
       <li>
         <a>Item 1</a>
       </li>
-      <li>
-        <a>Parent</a>
-        <ul className="p-2">
-          <li>
-            <a>Submenu 1</a>
-          </li>
-          <li>
-            <a>Submenu 2</a>
-          </li>
-        </ul>
-      </li>
+
       <li>
         <a>Item 3</a>
       </li>
