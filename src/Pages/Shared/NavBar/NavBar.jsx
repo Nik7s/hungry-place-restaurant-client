be8179eea230423dc6b714b294e9d46f16a08d5d
okshi <johnstarhafiz@gmail.com>
2023-09-14 21:03:40 +0600

    Create custom responsive  sub nav menu items

--- src/Pages/Shared/NavBar/NavBar.jsx
+++ src/Pages/Shared/NavBar/NavBar.jsx
@@ -1,5 +1,25 @@
 const NavBar = () => {
-  const subNavItems = <></>;
+  const navItems = (
+    <>
+      <li>
+        <a>Item 1</a>
+      </li>
+      <li>
+        <a>Parent</a>
+        <ul className="p-2">
+          <li>
+            <a>Submenu 1</a>
+          </li>
+          <li>
+            <a>Submenu 2</a>
+          </li>
+        </ul>
+      </li>
+      <li>
+        <a>Item 3</a>
+      </li>
+    </>
+  );
   return (
     <div className="navbar bg-base-100 text-white">
       <div className="navbar-start">
@@ -24,35 +44,13 @@ const NavBar = () => {
             tabIndex={0}
             className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
           >
-            <li>
-              <a>Item 1</a>
-            </li>
-            <li>
-              <a>Parent</a>
-              <ul className="p-2">{subNavItems}</ul>
-            </li>
-            <li>
-              <a>Item 3</a>
-            </li>
+            {navItems}
           </ul>
         </div>
         <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
       </div>
       <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">
-          <li>
-            <a>Item 1</a>
-          </li>
-          <li tabIndex={0}>
-            <details>
-              <summary>Parent</summary>
-              <ul className="p-2">{subNavItems}</ul>
-            </details>
-          </li>
-          <li>
-            <a>Item 3</a>
-          </li>
-        </ul>
+        <ul className="menu menu-horizontal px-1">{navItems}</ul>
       </div>
       <div className="navbar-end">
         <a className="btn">Button</a>
