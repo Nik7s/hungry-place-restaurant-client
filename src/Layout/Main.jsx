a4d29478432fe552ea81069041cd6659dd66cde3
okshi <johnstarhafiz@gmail.com>
2023-09-14 20:55:03 +0600

    Import basic  Navbar from Daisy-UI

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../Pages/Shared/Footer/Footer";
+import NavBar from "../Pages/Shared/NavBar/NavBar";
 
 const Main = () => {
   return (
     <div className="h-screen bg-white text-black">
+      <NavBar />
       <Outlet />
       <Footer />
     </div>
