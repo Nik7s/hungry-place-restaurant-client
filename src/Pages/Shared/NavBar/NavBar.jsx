d0a65115a21e9a0fe92684b6c6de1799fa67dd73
okshi <johnstarhafiz@gmail.com>
2023-09-15 22:15:41 +0600

    Navbar customise done

--- src/Pages/Shared/NavBar/NavBar.jsx
+++ src/Pages/Shared/NavBar/NavBar.jsx
@@ -12,7 +12,7 @@ const NavBar = () => {
     </>
   );
   return (
-    <div className="navbar fixed z-10 opacity-40 bg-black text-white">
+    <div className="navbar fixed z-10 bg-opacity-40 bg-black text-white">
       <div className="navbar-start ps-12">
         <div className="dropdown">
           <label tabIndex={0} className="btn btn-ghost lg:hidden">
