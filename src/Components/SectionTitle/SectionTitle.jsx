7fd052cd60e54e08e39b47e2ad39f8404d9235d6
okshi <johnstarhafiz@gmail.com>
2023-09-16 23:32:43 +0600

    Create Section title daynamic component

---
+++ src/Components/SectionTitle/SectionTitle.jsx
@@ -0,0 +1,10 @@
+const SectionTitle = ({ heading, subHeading }) => {
+  return (
+    <div>
+      <p>{subHeading}</p>
+      <h1>{heading}</h1>
+    </div>
+  );
+};
+
+export default SectionTitle;
