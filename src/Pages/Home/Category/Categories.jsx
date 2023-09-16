1a9c9a5cc89e87584a76e8afd19653a568afdc8b
okshi <johnstarhafiz@gmail.com>
2023-09-16 10:56:31 +0600

    Working on slider folder

--- src/Pages/Home/Category/Categories.jsx
+++ src/Pages/Home/Category/Categories.jsx
@@ -1,3 +1,8 @@
+import React, { useRef, useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+
 const Categories = () => {
   return (
     <div className="h-screen bg-white">
