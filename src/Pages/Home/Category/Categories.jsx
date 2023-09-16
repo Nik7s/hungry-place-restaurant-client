8ca146b4a54321e99412529d46333f125696aa18
okshi <johnstarhafiz@gmail.com>
2023-09-16 11:05:16 +0600

    Categories slider work successfully

--- src/Pages/Home/Category/Categories.jsx
+++ src/Pages/Home/Category/Categories.jsx
@@ -1,12 +1,40 @@
-import React, { useRef, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
-
+import { Pagination } from "swiper/modules";
+import s1 from "../../../../src/assets/slide/slide1.jpg";
+import s2 from "../../../../src/assets/slide/slide2.jpg";
+import s3 from "../../../../src/assets/slide/slide3.jpg";
+import s4 from "../../../../src/assets/slide/slide4.jpg";
+import s5 from "../../../../src/assets/slide/slide5.jpg";
 const Categories = () => {
   return (
-    <div className="h-screen bg-white">
-      <h1>This is Category</h1>
+    <div className="py-20 bg-white">
+      <Swiper
+        slidesPerView={3}
+        spaceBetween={30}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <img src={s1} alt="slide" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={s2} alt="slide" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={s3} alt="slide" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={s4} alt="slide" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={s5} alt="slide" />
+        </SwiperSlide>
+      </Swiper>
     </div>
   );
 };
