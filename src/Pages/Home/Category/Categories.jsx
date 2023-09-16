74470361c653c98e6c64570f1a0c42d0f892472e
okshi <johnstarhafiz@gmail.com>
2023-09-16 11:13:01 +0600

    Used AOS fade animation in category slider

--- src/Pages/Home/Category/Categories.jsx
+++ src/Pages/Home/Category/Categories.jsx
@@ -1,3 +1,6 @@
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
@@ -6,10 +9,21 @@ import s1 from "../../../../src/assets/slide/slide1.jpg";
 import s2 from "../../../../src/assets/slide/slide2.jpg";
 import s3 from "../../../../src/assets/slide/slide3.jpg";
 import s4 from "../../../../src/assets/slide/slide4.jpg";
-import s5 from "../../../../src/assets/slide/slide5.jpg";
+
 const Categories = () => {
+  //AOS animation used here
+  useEffect(() => {
+    AOS.init();
+    AOS.refresh();
+  }, []);
+
   return (
-    <div className="py-20 bg-white">
+    <div
+      data-aos="fade-up"
+      data-aos-duration="2000"
+      data-aos-easing="linear"
+      className="pt-10 pb-20 max-w-screen-lg	mx-auto bg-white"
+    >
       <Swiper
         slidesPerView={3}
         spaceBetween={30}
@@ -30,9 +44,6 @@ const Categories = () => {
         </SwiperSlide>
         <SwiperSlide>
           <img src={s4} alt="slide" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={s5} alt="slide" />
         </SwiperSlide>
       </Swiper>
     </div>
