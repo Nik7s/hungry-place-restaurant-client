4a31c623c7654e48accea2073272d0d8934e138c
okshi <johnstarhafiz@gmail.com>
2023-09-17 11:30:16 +0600

    Working to design Items menu

--- src/Pages/Shared/MenuItems/MenuItem.jsx
+++ src/Pages/Shared/MenuItems/MenuItem.jsx
@@ -1,8 +1,20 @@
+// eslint-disable-next-line react/prop-types
 const MenuItem = ({ item }) => {
-  console.log(item);
+  const { name, price, recipe, image } = item || {};
+
   return (
-    <div>
-      <p>item</p>
+    <div className="flex items-center justify-between ">
+      <img
+        style={{ borderRadius: "0 200px 200px 200px" }}
+        className="w-[118px] h-[104px]"
+        src={image}
+        alt="recipe image"
+      />
+      <div>
+        <p>{name}</p>
+        <p>{recipe}</p>
+      </div>
+      <p>$ {price}</p>
     </div>
   );
 };
