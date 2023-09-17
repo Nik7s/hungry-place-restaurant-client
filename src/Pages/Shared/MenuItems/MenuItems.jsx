19b69bc220a352f7bf2577e70a7d12fdf18ef341
okshi <johnstarhafiz@gmail.com>
2023-09-17 11:35:14 +0600

    Working to design Items menu

--- src/Pages/Shared/MenuItems/MenuItems.jsx
+++ src/Pages/Shared/MenuItems/MenuItems.jsx
@@ -15,9 +15,9 @@ const MenuItems = () => {
   }, []);
 
   return (
-    <section>
+    <section className="max-w-screen-lg	mx-auto">
       <SectionTitle subHeading="Check it out" heading="FROM OUR MENU" />
-      <div>
+      <div className="grid md:grid-cols-2 mt-4 pb-16 gap-4">
         {items.map((item) => (
           <MenuItem key={item._id} item={item} />
         ))}
