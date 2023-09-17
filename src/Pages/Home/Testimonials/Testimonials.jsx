a76bac30674dfeba7d7f7f184b2b2046b4fa451b
okshi <johnstarhafiz@gmail.com>
2023-09-17 17:01:51 +0600

    Istall reac rating package for show daymaci ratign

--- src/Pages/Home/Testimonials/Testimonials.jsx
+++ src/Pages/Home/Testimonials/Testimonials.jsx
@@ -1,5 +1,9 @@
+import { Swiper, SwiperSlide } from "swiper/react";
 import { useEffect, useState } from "react";
 import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
+import "swiper/css";
+import "swiper/css/navigation";
+import { Navigation } from "swiper/modules";
 
 const Testimonials = () => {
   const [reviews, setReviews] = useState([]);
@@ -16,6 +20,14 @@ const Testimonials = () => {
         subHeading={"What Our Clients Say"}
         heading={"Testimonials"}
       />
+      <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+        {reviews.map((review) => (
+          <SwiperSlide key={review._id}>
+            <p>{review?.details}</p>
+            <p>{review?.name}</p>
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </div>
   );
 };
