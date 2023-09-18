5b73a3ccddea374b572396d60b5d471636b2a608
okshi <johnstarhafiz@gmail.com>
2023-09-18 19:43:46 +0600

    Setup Router path to navigate Our Menu Component

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
 import Home from "../Pages/Home/Home/Home";
+import Menu from "../Pages/Menu/Menu/Menu";
 
 export const router = createBrowserRouter([
   {
@@ -10,6 +11,10 @@ export const router = createBrowserRouter([
       {
         path: "/",
         element: <Home />,
+      },
+      {
+        path: "menu",
+        element: <Menu />,
       },
     ],
   },
