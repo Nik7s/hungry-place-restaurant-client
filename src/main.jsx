102ad345d7ef73c597b43f1eb151696c43fa9922
okshi <johnstarhafiz@gmail.com>
2023-09-18 19:51:09 +0600

    Used Hamlet Provider insde the root file of app

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,12 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { router } from "./Routes/Routes.jsx";
 import { RouterProvider } from "react-router-dom";
+import { HelmetProvider } from "react-helmet-async";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <HelmetProvider>
+      <RouterProvider router={router} />
+    </HelmetProvider>
   </React.StrictMode>
 );
